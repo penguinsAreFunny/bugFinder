2612926a2522eca00a6aede73a380efb12f5c893
Robert Klinger <robert@k1inger.de>
2021-11-25 22:32:09 +0100

    first clean-up:renaming main-files

--- src/04a-training/src/pythonInterface.ts
+++ src/04a-training/src/pythonInterface.ts
@@ -1,5 +1,4 @@
-import {Trainer} from "./trainer";
-import {Dataset, SHARED_TYPES} from "bugfinder-framework";
+import {Dataset, Trainer} from "bugfinder-framework";
 import * as fs from "fs"
 import {execSync} from "child_process";
 import {inject, injectable} from "inversify";
@@ -19,14 +18,14 @@ export class PythonInterface implements Trainer{
         console.log("Feel free to model the whole training process, if you like to automate training.")
         console.log("This function will only dump the dataset to a json file so that you can train with the" +
             " given src templates or your own src scripts.")
-        console.log("Have a look at src/src/main.ipynb. Run conda activate ./ml_env to use the template scripts.")
+        console.log("Have a look at the package packages/bugFinder-machineLearning to use the template scripts.")
         console.log(`You can find the dataset as a json-file with path: ${datasetPathSrc}.`)
         console.log("Use the Python scripts to select and train a suitable model.")
 
-        const stdout1 = execSync("conda activate ./ml_env",{cwd: pythonWd})
-        const stdout2 = execSync(`python main.py ${datasetPath}`, {cwd: pythonWd})
-        console.log(stdout1.toString())
-        console.log(stdout2.toString())
+        //const stdout1 = execSync("conda activate ./ml_env",{cwd: pythonWd})
+        //const stdout2 = execSync(`python main.py ${datasetPath}`, {cwd: pythonWd})
+        //console.log(stdout1.toString())
+        //console.log(stdout2.toString())
         console.log("PythonWD: ", pythonWd)
         console.log("Datasetpath: ", datasetPath)
     }
